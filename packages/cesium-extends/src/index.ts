99fe2e719fe9c786ac9949dfab26d3e9635bfde8
hongfaqiu <951142905@qq.com>
2023-04-14 15:49:09 +0800

    fix: export Drawer

--- packages/cesium-extends/src/index.ts
+++ packages/cesium-extends/src/index.ts
@@ -4,6 +4,8 @@ export * from '@cesium-extends/subscriber';
 export * from '@cesium-extends/tooltip';
 export * from '@cesium-extends/drawer';
 export * from '@cesium-extends/measure';
+
+export { default as Drawer } from '@cesium-extends/drawer';
 export { default as Popup } from '@cesium-extends/popup';
 export { default as Compass } from '@cesium-extends/compass';
 export { default as ZoomControl } from '@cesium-extends/zoom-control';
